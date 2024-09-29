a2e47263494e15cf0c03ca8f57a8ac0b7049c330
IML1012 <118357948+iml1012@users.noreply.github.com>
2024-09-29 22:25:34 +0200

    Add files via upload

---
+++ UBULib/Utilidades.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBULib
+{
+    public class Utilidades{
+
+        // Función para encriptar una cadena de texto.
+        public string Encriptar(string password) {
+
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(password);
+            SHA256 mySHA256 = SHA256.Create();
+            bytes = mySHA256.ComputeHash(bytes);
+
+            return (System.Text.Encoding.ASCII.GetString(bytes));
+        }
+
+        // Función para comprobar el tipo de complejidad de una contraseña
+        public int CompruebaPassword(string password) {
+
+
+            string caracteres = "abcdefghijklmnñopqrstuvwxyz";
+            string numeros = "0123456789";
+            string especiales = "[]{}()!¡¿?=.,;:|@#$€+*/_-";
+
+            int contador = 0;
+
+            if (password.Length > 0) contador++;
+            if (password.Length < 16) contador++;
+            if (password.Intersect(caracteres).Count() > 0) contador++;
+            if (password.Intersect(numeros).Count() > 0) contador++;
+            if (password.Intersect(caracteres.ToUpper()).Count() > 0) contador++;
+            if (password.Intersect(especiales).Count() > 0) contador++;
+
+            return (contador);
+        }
+
+    }
+}
---
+++ UBULibTests/UtilidadesTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UBULib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBULib.Tests
+{
+    [TestClass()]
+    public class UtilidadesTests
+    {
+        [TestMethod()]
+        public void EncriptarTest()
+        {
+
+            string password0 = "Pa$$w0rd";
+            string password1 = "Pa$$w0rd";
+            string password2 = "";
+            string password3 = "P@$$w0rd";
+
+            Utilidades utilidades = new Utilidades();
+
+            Assert.AreEqual(utilidades.Encriptar(password0), utilidades.Encriptar(password1));
+            Assert.AreNotEqual(utilidades.Encriptar(password0), utilidades.Encriptar(password2));
+            Assert.AreNotEqual(utilidades.Encriptar(password0), utilidades.Encriptar(password3));
+        }
+
+        [TestMethod()]
+        public void CompruebaPasswordTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
